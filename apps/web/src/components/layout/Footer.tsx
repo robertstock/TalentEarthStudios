2b08f0b88c28dbe72ea694d3881c85150cd13651
Robert Stock <stockfamily@Roberts-MacBook-Pro.local>
2026-02-17 10:07:53 -0800

    Update footer logo

--- apps/web/src/components/layout/Footer.tsx
+++ apps/web/src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-const LogoImage = "/talentearth_logo.png";
+const LogoImage = "/TalentEarthStudioLogo_White.png";
 
 export default function Footer() {
     return (
